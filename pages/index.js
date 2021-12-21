39c7b50d6ad56424bdc80063226c2fa7f9962a2a
mtskei <michel.tcha@gmail.com>
2021-12-21 21:17:52 +0100

    audiophile project init

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+	return (
+		<section>
+			<h1 className="text-blue-500 font-bold text-5xl">Let's gooo</h1>
+			<p className="text-gray-300">Hello</p>
+		</section>
+	);
+}
